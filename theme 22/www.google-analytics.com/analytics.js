b4fc703224af87a1b4ee6db239d8089494ad4b99
Khuat Quang Hung <hungsct1702@gmail.com>
2025-02-26 15:41:31 +0700

    commit

---
+++ theme 22/www.google-analytics.com/analytics.js
@@ -0,0 +1,14 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from www.google-analytics.com/analytics.js by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 26 Feb 2025 03:06:01 GMT -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=http://172.16.50.1:8183/redirect.cgi?u=www.google-analytics.com%26c=Adware"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="http://172.16.50.1:8183/redirect.cgi?u=www.google-analytics.com%26c=Adware"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from www.google-analytics.com/analytics.js by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 26 Feb 2025 03:06:01 GMT -->
+</HTML>
